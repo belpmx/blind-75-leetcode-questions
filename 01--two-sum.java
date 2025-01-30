09981fdf9e65d83a9940e7af8d35842a1ccbf20e
Pabel <iscpabel@gmail.com>
2025-01-30 15:09:47 -0600

    Add: Solution for two Sum

--- 01--two-sum.java
+++ 01--two-sum.java
@@ -1,4 +1,49 @@
-class Solution {
+import java.util.HashMap;
+class MySolution01{
+    public int[] twoSum(int[] nums, int target) {
+        //create HashMap to store the array elements
+        HashMap<Integer, Integer> storageMap = new HashMap<>();
+        //to store the result
+        int[] index = new int[2];
+        for (int i =0; i< nums.length; i++){
+            int complement = target -nums[i];
+            //if solution is not found
+            if (!storageMap.containsKey(complement)){
+            //store the current element as already visited with the index
+                storageMap.put(nums[i], i);
+            }
+            else {
+                //A solution is found then need to store the indices of numbers on result array
+                //find the index of the complement
+                index[0] = storageMap.get(complement);
+                index[1] = i;
+                break;
+            }
+        }
+        return index;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution01 {
     public int[] twoSum(int[] nums, int target) {
         int[] ans = new int[2];
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -14,3 +59,4 @@ class Solution {
         return ans;
     }
 } // TC: O(n), SC: O(n)
+
