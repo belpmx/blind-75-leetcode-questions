ee4c07bbbc99e6c7e6ab3151c72277b3b8dc64a7
Pabel <iscpabel@gmail.com>
2025-03-03 10:49:41 -0600

    Add: Valid Anagram, rework need it on 53

--- 12--number-of-1-bits.java
+++ 12--number-of-1-bits.java
@@ -1,4 +1,29 @@
-public class Solution {
+
+class Solution012 {
+    public int hammingWeight(int n) {
+
+        return 0;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution12 {
     public int hammingWeight(int n) {
         int count = 0;
         for (int i = 0; i < 32; i++) {
--- 53--valind-anagram.java
+++ 53--valind-anagram.java
@@ -1,4 +1,89 @@
-class Solution {
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution053 {
+    //non working, need to troubleshoot
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+        HashMap<String, Integer> mapS = new HashMap<>();
+        HashMap<String, Integer> mapT = new HashMap<>();
+        for (int i = 0; i<s.length();i++){
+            //Store both strings into Hash map
+            //Key is the char, value is the count of occurrences of the char
+            if (mapS.containsKey(s.charAt(i))){
+                int value = mapS.get(s.charAt(i)) +1;
+                mapS.replace(String.valueOf((s.charAt(i))),value);
+            } else {
+                mapS.put(String.valueOf((s.charAt(i))),1);
+            }
+
+            if (mapT.containsKey(t.charAt(i))){
+                int value = mapT.get(t.charAt(i)) +1;
+                mapT.replace(String.valueOf((t.charAt(i))),value);
+            } else {
+                mapT.put(String.valueOf((t.charAt(i))),1);
+            }
+        }
+
+        for (Map.Entry<String,Integer> entry :mapS.entrySet()){
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            if (mapT.containsKey(key)){
+              if (mapT.get(key) != value){
+                  return false;
+              }
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+class Solution0053 {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+        //Create an array to hold each of the characters of ascii chars
+        int[] mapChars = new int [26];
+        //For s iterate and increment by one for each occurrence
+        //For t iterate and decrement by one for each occurrence
+        for (int i = 0; i<s.length(); i++){
+            char charS = s.charAt(i);
+            char charT = t.charAt(i);
+            mapChars[charS - 'a']++;
+            mapChars[charT - 'a']--;
+        }
+        //Scan array and all elements must be zero
+        for(int i =0; i< mapChars.length;i++){
+            if(mapChars[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution53 {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) return false;
         int[] map = new int[26];
