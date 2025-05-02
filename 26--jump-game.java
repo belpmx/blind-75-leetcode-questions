c2637a71f4f4ec76145cd6447db100a3b40b54d8
Pabel <iscpabel@gmail.com>
2025-05-02 13:51:22 -0600

    Add: Palindrome substrings Jump Game

--- 26--jump-game.java
+++ 26--jump-game.java
@@ -1,4 +1,29 @@
-class Solution {
+class Solution0026 {
+    public boolean canJump(int[] nums) {
+        int reachable =0;//at the start we can reach position 0
+        for (int i=0;i< nums.length;i++){
+            //validate if in this iteration we can still reach the end of the array
+            if(i>reachable){
+                return false;
+            }
+            //i holds the index that we are in, then we add the value of the element at this index
+            reachable = Math.max(reachable, i+nums[i]);
+            if(reachable>= nums.length-1){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+
+
+
+
+
+
+
+class Solution026 {
     public boolean canJump(int[] nums) {
         int reachable = 0;
         for (int i = 0; i < nums.length && i <= reachable; i++) {
--- 58--palindrome-substrings.java
+++ 58--palindrome-substrings.java
@@ -1,4 +1,43 @@
-class Solution {
+class Solution058 {
+    public int countSubstrings(String s) {
+        int count =0;
+        for (int i=0; i<s.length();i++){
+            //need to call for each character, when L and R are the same
+            count += find(s,i,i);
+            // need to call for each character +1
+            count += find(s, i, i+1);
+        }
+        return count;
+    }
+
+    public int find (String s, int left, int right){
+        int count = 0;
+        // left >= 0 && right <s.length() -> to determine if pointer are in valid range
+        while (left >= 0 && right <s.length()&&s.charAt(left) == s.charAt(right)){
+            count++;
+            left --;
+            right ++;
+        }
+        return count;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution58 {
     public int countSubstrings(String s) {
         int count = 0;
         for (int i = 0; i < s.length(); i++) {
